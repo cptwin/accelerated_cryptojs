b714be48b7575ce372927dcea1db2ea7465e112a
Dajne <dajnewin@gmail.com>
2016-05-17 12:05:13 +1200

    NTRUEncryptJS-GPU Tests

---
+++ gpu/fragmentShaderDecryptionConvolution677.js
@@ -0,0 +1,33 @@
+/* 
+ * Copyright 2015 dwin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+document.write('\
+        <script id="fragmentShaderDecryptionConvolution677" type="x-shader/x-fragment">\
+			uniform float cipherText[677];\
+			uniform float privateF[677];\
+					void main() {\
+						vec2 stCoord = vec2(gl_FragCoord.x,gl_FragCoord.y);\
+						int coordInt = int(floor(stCoord.x));\
+						for(int i = 0; i < 677; i++) {\
+							for(int j = 0; j < 677; j++) {\
+								if(int(floor(mod(float(i+j),float(677)))) == coordInt) {\
+									gl_FragColor.r += cipherText[i] * privateF[j];\
+								}\
+							}\
+						}\
+					}\
+        </script>\
+');
---
+++ gpu/fragmentShaderEncryptionAddition613.js
@@ -0,0 +1,42 @@
+/* 
+ * Copyright 2015 dwin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+document.write('\
+        <script id="fragmentShaderEncryptionAddition613" type="x-shader/x-fragment">\
+			uniform sampler2D sTexture;\
+			uniform float message[613];\
+					float modulus(float value, float modulus) {\
+						float outputValue = mod(floor(value), floor(modulus));\
+						if(outputValue < 0.0) {\
+							outputValue += modulus;\
+						}\
+						return floor(outputValue);\
+					}\
+					void main() {\
+						vec2 stCoord = vec2(gl_FragCoord.x,gl_FragCoord.y);\
+						int coordInt = int(floor(stCoord.x));\
+						vec4 inputTextureAtThisPixel = texture2D(sTexture,vec2(gl_FragCoord.x/613.0,0.5));\
+						gl_FragColor.r = inputTextureAtThisPixel.r;\
+						for(int i = 0; i < 613; i++) {\
+							if(coordInt == i) {\
+								gl_FragColor.r = inputTextureAtThisPixel.r;\
+								gl_FragColor.g += float(int(floor(modulus(float(inputTextureAtThisPixel.r + message[i]),float(2048)))));\
+								gl_FragColor.b = message[i];\
+							}\
+						}\
+					}\
+        </script>\
+');
